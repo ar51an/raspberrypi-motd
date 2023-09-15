c3afcb8c752058da0b5d77cbe721b8d7f2c5ea4f
AA <arsl99@gmail.com>
2023-09-15 02:55:52 -0400

    Removed unicode arrows

--- update-os/update.sh
+++ update-os/update.sh
@@ -4,17 +4,17 @@
 # It will also update the Static Motd File [20-update]
 # for displaying upgradeable package count at startup
 
-{ echo -e "\e[30;48;5;248m⮞⮞ Update Packages ⮜⮜\e[0m"; } 2> /dev/null
+{ echo -e "\e[30;48;5;248mUpdate Packages\e[0m"; } 2> /dev/null
 sudo apt update
 
-{ echo -e "\n\e[30;48;5;248m⮞⮞ Full Upgrade Packages ⮜⮜\e[0m"; } 2> /dev/null
+{ echo -e "\n\e[30;48;5;248mFull Upgrade Packages\e[0m"; } 2> /dev/null
 sudo apt full-upgrade
 
-{ echo -e "\n\e[30;48;5;248m⮞⮞ Remove Dependency Packages That Are No Longer Needed ⮜⮜\e[0m"; } 2> /dev/null
+{ echo -e "\n\e[30;48;5;248mRemove Dependency Packages That Are No Longer Needed\e[0m"; } 2> /dev/null
 sudo apt --purge autoremove
 
-{ echo -e "\n\e[30;48;5;248m⮞⮞ Clean apt Cache ⮜⮜\e[0m"; } 2> /dev/null
+{ echo -e "\n\e[30;48;5;248mClean apt Cache\e[0m"; } 2> /dev/null
 sudo apt clean
 
-{ echo -e "\n\e[30;48;5;248m⮞⮞ Update Static Motd - Dynamic Folder File /etc/update-motd.d/20-update ⮜⮜\e[0m"; } 2> /dev/null
+{ echo -e "\n\e[30;48;5;248mUpdate Static Motd - Dynamic Folder File /etc/update-motd.d/20-update\e[0m"; } 2> /dev/null
 sudo run-parts /etc/update-motd-static.d
